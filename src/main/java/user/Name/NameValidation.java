bcc587c66af442d6fad63cab766b96716a525a81
harshrastogi15 <hrastogi2002@gmail.com>
2023-12-05 19:15:45 +0530

    Refactored: Update for email validation

--- src/main/java/user/Name/NameValidation.java
+++ src/main/java/user/Name/NameValidation.java
@@ -8,6 +8,8 @@ public class NameValidation {
         System.out.println("Function for Validation");
         boolean flag = nameValidationRegex("Abb");
         System.out.println(flag);
+        boolean flag2 = emailValidationRegex("Abb.xyzm.com");
+        System.out.println(flag2);
     }
 
     public static boolean nameValidationRegex(String st){
@@ -16,4 +18,19 @@ public class NameValidation {
         Matcher matcher = pattern.matcher(st);
         return matcher.matches();
     }
+
+    /*
+    *  UC-3
+    *   As a User need to enter a valid email
+    *   E.g. abc.xyz@bl.co.in - Email has 3 mandatory
+    *   parts (abc, bl & co) and 2 optional (xyz & in) withprecise @ and . positions
+    * */
+
+    public static boolean emailValidationRegex(String st){
+        String regex = "^[a-zA-Z0-9]+(.[a-zA-Z0-9]+)*@[a-zA-Z](.[a-zA-Z]+)+$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(st);
+        return matcher.matches();
+    }
+
 }
